650c011ae067020afe30d8a39e79eae35ce662bf
pyrokine <pyrokinecist@aliyun.com>
2020-10-28 15:41:59 +0800

    更换为json的方式保存

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
-# 快速获取微信文章的标题及链接导出到xlsx
-
-> 起因：关注了几个优质的公众号进行学习，发现PC端的微信在翻阅历史文章的操作上严重缺乏用户体验。
->
-> 目的：将作者所有的历史文章的标题及链接保存到xlsx方便查阅
+# 快速获取微信文章的标题、作者、链接和时间并导出到json
 
 ## 项目地址
 
-https://github.com/iicey/mitm
+原项目地址  
+https://github.com/iicey/mitm  
+本项目地址  
+https://github.com/Pyrokine/mitm  
 
 ## 使用说明
 
@@ -19,17 +18,17 @@ pip install mitmproxy
 
 ### 设置代理(127.0.0.1:8080)
 
-![1564572090441](https://github.com/iicey/mitm/blob/master/img/1564572090441.png)
+![1564572090441](https://github.com/Pyrokine/mitm/blob/master/img/1564572090441.png)
 
-![1564572149417](https://github.com/iicey/mitm/blob/master/img/1564572149417.png)
+![1564572149417](https://github.com/Pyrokine/mitm/blob/master/img/1564572149417.png)
 
 ### 安装证书
 
-访问http://mitm.it/	安装Windows证书
+访问 http://mitm.it/	安装Windows证书，如果提示流量未经过 mitm ，可以尝试更换浏览器，比如 Chrome 浏览器  
 
 ### 启动脚本
 
-开启cmd切换到mitm目录下，执行下面这段代码
+开启 cmd 或 git bash 切换到当前目录下，执行下面这段代码（使用 mitmweb 替换 mitmdump 开启带界面的模式）
 
 ```
 mitmdump -s script.py
@@ -37,6 +36,6 @@ mitmdump -s script.py
 
 ### 最后一步
 
-点开PC微信里微信公众号的列表页，向下滑动即可
+点开PC微信里微信公众号的列表页，向下滑动即可（必须从这个位置打开单独的公众号窗口，否则是抓取不到的）
 
-![1564572879319](https://github.com/iicey/mitm/blob/master/img/1564572879319.png)
+![1564572879319](https://github.com/Pyrokine/mitm/blob/master/img/1564572879319.png)
--- script.py
+++ script.py
@@ -1,8 +1,8 @@
 import json
-
 import time
 import mitmproxy.http
-from openpyxl import Workbook, load_workbook
+from mitmproxy import ctx
+import os
 
 
 def request(flow: mitmproxy.http.HTTPFlow):
@@ -10,9 +10,13 @@ def request(flow: mitmproxy.http.HTTPFlow):
 
 
 def response(flow: mitmproxy.http.HTTPFlow):
+    # ctx.log.info(flow.request.url)
     if 'profile_ext' in flow.request.url:
+        ctx.log.info("gotcha")
         result = json.loads(flow.response.get_text())
         general_msg_list = result.get('general_msg_list')
+
+        data = {}
         for i in json.loads(general_msg_list).get('list'):
             app_msg_ext_info = i.get('app_msg_ext_info')
             title = app_msg_ext_info.get('title')
@@ -21,10 +25,19 @@ def response(flow: mitmproxy.http.HTTPFlow):
             comm_msg_info = i.get('comm_msg_info')
             datetime = comm_msg_info.get('datetime')
             publish_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(datetime))
-            try:
-                wb = load_workbook(''.join([author, '.xlsx']))
-            except FileNotFoundError:
-                wb = Workbook()
-            ws = wb.active
-            ws.append([publish_time, title, content_url])
-            wb.save(filename=''.join([author, '.xlsx']))
+            data.update({title: {"time": publish_time, "author": author, "url": content_url}})
+
+        # 改成公众号的名字用于保存爬取到的内容
+        nickname = "CSDN"
+        file_path = "content/" + nickname + ".json"
+        if not os.path.exists(file_path):
+            with open(file_path, "w", encoding="utf-8") as f:
+                json.dump({}, f, ensure_ascii=False)
+
+        with open(file_path, "r", encoding="utf-8") as f:
+            content = json.load(f)
+
+        content = dict(content, **data)
+
+        with open(file_path, "w", encoding="utf-8") as f:
+            json.dump(content, f, ensure_ascii=False)
